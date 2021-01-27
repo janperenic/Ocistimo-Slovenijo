3ae2423cb3768f6d14c4be05f57acbb037606d7f
Jure Rotar <jrotar0@gmail.com>
2021-01-27 21:07:37 +0100

    Added description

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,10 @@ use TimoKoerber\LaravelJsonSeeder\JsonDatabaseSeeder as Seed;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * runs seed found in database/json
+     * tests with single instance of each model found in database/json/tests
+     */
     public function run()
     {
         $this->call(Seed::class);
